e3d3eb542473abaec64d2c9a4df38235fb07599e
BabkaOrk <xm8q@bk.ru>
2020-04-27 20:59:26 +0300

    Added practice 3: udp client-server
    Signed-off-by: BabkaOrk <xm8q@bk.ru>

--- Makefile
+++ Makefile
@@ -3,5 +3,8 @@ fork:
 shm:
 	gcc -o shm_write practice_2_sharedm_write.c -lrt 
 	gcc -o shm_print practice_2_sharedm_print.c -lrt 
+udp:
+	gcc -o server practice_3_udp_server.c
+	gcc -o client practice_3_udp_client.c
 clean:
-	rm fork shm_print shm_write
+	rm fork shm_print shm_write client server
---
+++ practice_3_udp_client.c
@@ -0,0 +1,48 @@
+// Client side implementation of UDP client-server model 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define PORT 		1234
+#define MAXLINE 	1024
+
+
+int main(int argc, char ** argv) {
+	int sockfd;
+	char buffer[MAXLINE];
+	char *msg = "";
+	struct sockaddr_in	 servaddr;
+
+	if (argc > 1){
+		msg = argv[1];
+	} else{
+		printf("Using: ./client 'Start' or ./client 'Stop'\n");
+		return 1;
+	}
+
+	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+		perror("socket creation failed");
+		exit(EXIT_FAILURE);
+	} 
+
+	memset(&servaddr, 0, sizeof(servaddr));
+
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(PORT);
+	servaddr.sin_addr.s_addr = INADDR_ANY;
+
+	int n, len;
+	sendto(sockfd, (const char *)msg, strlen(msg),
+		MSG_CONFIRM, (const struct sockaddr *) &servaddr,
+			sizeof(servaddr));
+
+	printf("'%s' message sent.\n", msg);
+
+	close(sockfd);
+	return 0;
+} 
